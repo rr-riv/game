f9968289f3e3649266f23ba3f5524de51f6e11b4
rr-riv <rivesten@live.jp>
2026-06-17 13:32:25 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/rivkey2/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xea, 0x8a, 0xf7, 0x63, 0x18, 0x4a, 0xc1, 0x88}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
